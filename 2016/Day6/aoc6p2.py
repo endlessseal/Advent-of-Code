19ec718c9d3bfaeaa25f0e7796bef5d256a195de
endlessseal <alfred.chan15@gmail.com>
2016-12-06 11:12:39 -0500

    Add files via upload

---
+++ 2016/Day6/aoc6p2.py
@@ -0,0 +1,12 @@
+import string
+
+x = [{a:0 for a in string.ascii_lowercase} for x in range(8)]
+answer = ''
+with open('aoc6.txt','r') as fp:
+    for each_line in fp:
+        for index,letters in enumerate(each_line.strip('\n')):
+            x[index][letters] += 1
+for every_letter in x:
+    answer += min(every_letter, key= lambda k: every_letter[k])
+    
+print(answer)
