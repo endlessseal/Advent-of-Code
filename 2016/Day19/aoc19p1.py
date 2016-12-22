fd970b742f8aed65199fe7074bb3f38a98b6d0e4
endlessseal <alfred.chan15@gmail.com>
2016-12-22 16:36:16 -0500

    Update aoc19p1.py

--- 2016/Day19/aoc19p1.py
+++ 2016/Day19/aoc19p1.py
@@ -1,5 +1,4 @@
 from collections import deque
-import time
 number_of_elves = 3018458
 list_of_elves = deque(list(range(1,number_of_elves+1)))
 
@@ -9,6 +8,4 @@ def solve():
             return list_of_elves[0]
         list_of_elves.rotate(-1)
         list_of_elves.popleft()
-x = time.time()
 print(solve())
-print(time.time()-x)
