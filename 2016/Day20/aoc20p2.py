f28a19b41c9202234c6d140d8308326c9d0ac253
endlessseal <alfred.chan15@gmail.com>
2016-12-22 16:38:12 -0500

    Add files via upload

---
+++ 2016/Day20/aoc20p2.py
@@ -0,0 +1,23 @@
+
+def construct_list():
+    list_of_ranges = []
+    with open('aoc20.txt', 'r') as fp:
+        for each_line in fp.read().splitlines():
+            lower,upper = map(int,each_line.split('-'))
+            list_of_ranges.append((lower,upper))
+        list_of_ranges.sort()
+    return list_of_ranges
+
+def find_smallest(list_of_ranges):
+    floor = 0
+    answers = set()
+    for each in list_of_ranges:
+        if floor < each[0]:
+            for every in range(floor,each[0]):
+                answers.add(every)
+        floor = max(each[1] + 1, floor) 
+    return len(answers)
+
+
+print(find_smallest(construct_list()))
+
