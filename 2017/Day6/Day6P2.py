4a031e337dd42d988e9a3b622c704029ffcd429b
endlessseal <alfred.chan15@gmail.com>
2017-12-08 16:40:43 -0500

    Add files via upload

---
+++ 2017/Day6/Day6P2.py
@@ -0,0 +1,26 @@
+data = '''0	5	10	0	11	14	13	4	11	8	8	7	1	4	12	11'''
+
+
+def solve(x):
+    memory = [int(y) for y in x.split('\t')]
+    memory_count = len(memory)
+    count = 0
+    seen_pattern = []
+    answer = None 
+    
+    while True:
+        mem_block_amount = max(memory)
+        mem_block_index = memory.index(mem_block_amount)
+        memory[mem_block_index] = 0
+        for _ in range(mem_block_amount):
+            mem_block_index += 1
+            memory[mem_block_index % memory_count] += 1
+        count += 1
+        if memory in seen_pattern and not answer:
+            answer = memory[:]
+            count = 0
+        if seen_pattern.count(answer) == 3:
+            return count-1 
+        seen_pattern.append(memory[:])
+    
+print(solve(data))
